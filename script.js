3bc05dd524ca67cf2a6aff385edfc614623dde51
Chad <cpiotrovsky@att.net>
2023-03-28 13:47:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Create four blocks, each of a different color (green, red, yellow, blue)
+// Audio attached to each block that plays when clicked 
+
+// Create difficulty modes or just one mode??????
+// Create start button 
+
+// Create a next level/round button
+
+// Create a reset button 
+
+// Create array to hold computer sequence
+// Create function to randomly generate the sequence 
+
+// Click event
+// How to display the computer sequence -  Loop through sequence array and highlight color based on each index number?? 
+// Create array to hold player's sequence/choice  
+// If/else for correct choice - Player's sequence === computer's sequence???
+    // How to add new piece to sequence based on IF passed level???
+        // Next round becomes visible if correct choice(s) made 
+
