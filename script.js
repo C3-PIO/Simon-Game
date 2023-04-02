1de4877a14c37a697c4d7b6e0a73770a059d38d5
Chad <cpiotrovsky@att.net>
2023-04-02 16:32:06 -0400

    added button to toggle block sound on/off

--- script.js
+++ script.js
@@ -41,16 +41,18 @@
 // 6. Lock the controls while the computer is playing with the same boolean
 
 //--------------CODE---------------------
-// HTML variable assignments 
+// DOM variable assignments 
 const startBtn = document.querySelector('#start')
 const nextBtn = document.querySelector('#next')
-const resetBtn = document.querySelector('#reset')
+// const resetBtn = document.querySelector('#reset')
+const muteBtn = document.querySelector('#mute')
 const h1El = document.querySelector('h1')
 const instEl = document.querySelector('.instructions')
 const blockContainer = document.querySelector('#blocks')
-const blocksEl = blockContainer.querySelectorAll('button')
+const blocksEl = blockContainer.querySelectorAll('.block')
+const audioEl = document.querySelectorAll('audio')
 
-//Block objects
+// Object for each colored block 
 const greenBlock = {
     element: document.getElementById('green'),
     index: 0,
@@ -59,7 +61,7 @@ const greenBlock = {
         this.audio.playbackRate = 1.3
         this.audio.play()
     },
-    // Clear block activation effect
+    // Removes block activation effects
     clearEffect(){
         this.element.style.boxShadow = ''
         this.element.style.backgroundColor = ''
@@ -108,20 +110,35 @@ const blueBlock = {
     }
 }
 
+// Click event for mute button to toggle block audio on/off
+let counter = 0
+muteBtn.addEventListener('click', ()=>{
+    counter ++
+    for (i=0; i<audioEl.length; i++){
+        if (counter % 2 != 0){
+            audioEl[i].muted = true
+            muteBtn.textContent = 'Sound OFF'
+        } else {
+            audioEl[i].muted = false 
+            muteBtn.textContent = 'Sound ON'
+        }
+    }
+})
+
 // Array of block objects 
 blocksArr = [greenBlock, redBlock, yellowBlock, blueBlock]
 
-// Game sequence array and function to generate random block for the sequence  
+// Game sequence array and function to generate random block index for the sequence  
 let gameSequence = []
 function randomBlock(){
     block = blocksArr[Math.floor(Math.random() * blocksArr.length)].index
     gameSequence.push(block)
 }
 
-// Player's sequence. Number's added based on block selection 
+// Player's sequence. Number's added based on user's block selection 
 let playerSequence = []
 
-// Disable buttons 
+// Disables blocks 
 function disableBtn(){
     for(i=0;i<blocksEl.length;i++){
         blocksEl[i].disabled = true // disables block clicks
@@ -150,22 +167,22 @@ function timer(){
     }, 1000)
 }
 
-// Switch statement to check game sequence's index array so the corresponding block with play it's audio and highlight 
+// This is where the magic happens. The for loop and switch statement loop through and check the game sequence index array to fire the corresponding block's "activation effect", i.e. play the block audio, add the block shadow effect, and highlight the block. 
 function activateGameSequence(){
     nextBtn.style.visibility = 'hidden' // re-hide next round button
     disableBtn() // Disables blocks so player cannot interrupt the game sequence 
-    let delayMultiplier = 600 // delay between each blocks activation 
+    let delayMultiplier = 600 // variable used to create delay between each blocks activation 
     for (let i=0; i<gameSequence.length; i++){
-        switch (gameSequence[i]){ // switch statement parameter to check each piece of the game sequence
+        switch (gameSequence[i]){ // switch parameter set to each element of the game sequence thanks to the for loop. Each case is based on the blocks index number 
         case 0: // index 0 corresponds with the green block, so the green blocks animation will begin  
-            setTimeout(()=>{greenBlock.clearEffect()}, delayMultiplier)
+            setTimeout(()=>{greenBlock.clearEffect()}, delayMultiplier) // clears animation effect soon after it fires  
             setTimeout(()=>{
                 greenBlock.playAudio()
-                greenBlock.element.style.boxShadow = '-20px -20px 15px white'
-                greenBlock.element.style.backgroundColor = 'rgb(0, 180, 0)'
-            }, delayMultiplier - 200)
+                greenBlock.element.style.boxShadow = '-20px -20px 15px white' // add's shadow effect 
+                greenBlock.element.style.backgroundColor = 'rgb(0, 180, 0)' // add's highlight effect 
+            }, delayMultiplier - 300) 
             break;
-        case 1:
+        case 1: // index 1 corresponds with the red block
             setTimeout(()=>{redBlock.clearEffect()}, delayMultiplier)
             setTimeout(()=>{
                 redBlock.audio.volume = .25
@@ -174,7 +191,7 @@ function activateGameSequence(){
                 redBlock.element.style.backgroundColor = 'red'
             }, delayMultiplier - 300)
             break;
-        case 2:
+        case 2: // index 1 corresponds with the yellow block
             setTimeout(()=>{yellowBlock.clearEffect()}, delayMultiplier)
             setTimeout(()=>{
                 yellowBlock.audio.volume = .25
@@ -183,7 +200,7 @@ function activateGameSequence(){
                 yellowBlock.element.style.backgroundColor = 'yellow'
             }, delayMultiplier - 300)
             break;
-        case 3:
+        case 3: // index 1 corresponds with the blue block
             setTimeout(()=>{blueBlock.clearEffect()}, delayMultiplier)
             setTimeout(()=>{
                 blueBlock.audio.volume = .25
@@ -195,8 +212,8 @@ function activateGameSequence(){
         }
         delayMultiplier += 600
     }
-    setTimeout(()=>{enableBtn()}, delayMultiplier - 500)
-    setTimeout(()=>{timer()}, delayMultiplier)
+    setTimeout(()=>{enableBtn()}, delayMultiplier - 500) // returns block access after game sequence is finished playing through 
+    setTimeout(()=>{timer()}, delayMultiplier) // starts 5 second timer 
 }
 
 function checkSequence(){
@@ -214,7 +231,7 @@ function checkSequence(){
     }
 } 
 
-//Click event for start button. Creates first piece of sequence array to begin game flow 
+//Click event for start button. Hides start button and instructions and creates first piece of game sequence array to begin game flow 
 startBtn.addEventListener('click', () => {
     startBtn.style.visibility = 'hidden'
     instEl.style.visibility = 'hidden'
@@ -223,7 +240,7 @@ startBtn.addEventListener('click', () => {
     h1El.textContent = `Level ${gameSequence.length} of 15`
 })
 
-// Click event for next button
+// Click event for next button. Clears player sequence from previoius round and adds new block to the game sequence. 
 nextBtn.addEventListener('click', () => {
     playerSequence = []
     randomBlock()
@@ -231,12 +248,12 @@ nextBtn.addEventListener('click', () => {
     activateGameSequence()
 })
 
-// Click events for each block. Corresponding block's audio plays and block index number is pushed into the playerSequence array
+// Click events for each block. Corresponding block's audio plays and block index number is pushed into the playerSequence array. Block highlight and shadow effect occur through CSS hover & active styling. 
 greenBlock.element.addEventListener('click', () => {
     greenBlock.audio.volume = 1.0
     greenBlock.playAudio()
     playerSequence.push(greenBlock.index)
-    clearInterval(time)
+    clearInterval(time) // stops timer 
     checkSequence()
 })
 
